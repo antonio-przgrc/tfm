4735fa7f124d08b65d06393083ee98d890b3fb41
Antonio Pérez García <antoniopgarcia95@gmail.com>
2024-08-19 21:02:07 +0200

    nuevos modelos training

--- code/1-training.py
+++ code/1-training.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 from darts import TimeSeries
 from darts.utils.callbacks import TFMProgressBar
 from darts.dataprocessing.transformers import Scaler
-from darts.models import RNNModel, TransformerModel, Prophet, BlockRNNModel, NBEATSModel
+from darts.models import RNNModel, TransformerModel, Prophet, BlockRNNModel, NBEATSModel, NHiTSModel, TCNModel, TFTModel, DLinearModel, NLinearModel, TiDEModel, TSMixerModel
 from darts.metrics import rmse
 from darts.utils.statistics import check_seasonality, plot_acf
 import darts.utils.timeseries_generation as tg
@@ -26,7 +26,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 def tratamiento(fichero):
-    dataframe = pd.read_excel(fichero)
+    dataframe = pd.read_csv(fichero)
     dataframe = dataframe.rename({'clave1':'fecha', 'uniTotal':'unidades'}, axis=1)
     dataframe['fecha'] = pd.to_datetime(dataframe['fecha'], format="%Y%m%d")
     dataframe = dataframe.set_index(['fecha'])
@@ -35,7 +35,7 @@ def tratamiento(fichero):
     dataframe = dataframe.groupby(pd.Grouper(freq='B'))
     dataframe = dataframe.sum()
     dataframe[dataframe['unidades'] < 0] = 0
-    dataframe = dataframe["2010-01-01":"2024-06-30"]
+    dataframe = dataframe["2012-01-01":"2024-06-30"]
     name = fichero[fichero.find('/')+1:fichero.find('.')]
     return name, dataframe
 
@@ -57,14 +57,15 @@ def grafico(dataframe):
     ax.grid(which='minor', alpha = 0.25, linestyle='--')
     fig.autofmt_xdate()
 
-df, names = agrupar(['data/filtros.xlsx', 'data/baterias.xlsx', 'data/discos.xlsx', 'data/pastillas.xlsx'])
+df, names = agrupar(['data/filtros.csv', 'data/baterias.csv', 'data/aceites.csv', 'data/limpiaparabrisas.csv'])
 
 df2 = pd.read_csv('data/meteo_olvera.csv', decimal=',')
 df2['fecha'] = pd.to_datetime(df2['fecha'], format="%Y-%m-%d")
 df2.set_index('fecha', inplace=True)
-df2 = df2[['tmed', 'tmin', 'tmax', 'prec', 'dir', 'velmedia', 'hrMedia']]
+df2 = df2[['tmed', 'prec', 'hrMedia']]
 df2 = df2.resample('B').first()
 df2 = df2.fillna(method='backfill')
+df2 = df2['2012':]
 df = pd.concat([df, df2], axis=1)
 
 df = df.reset_index()
@@ -73,10 +74,9 @@ train_df = df[:-130]
 test_df = df[-130:]
 
 scaler = MinMaxScaler(feature_range=(0, 1))
-#scaler.fit(train_df[['unidades']])
 
 # Definicion TimeSeries
-cols = ['tmed', 'tmin', 'tmax', 'prec', 'dir', 'velmedia', 'hrMedia']
+cols = ['tmed','prec', 'hrMedia']
 
 train = TimeSeries.from_dataframe(train_df, time_col='fecha', value_cols=names+cols)
 test = TimeSeries.from_dataframe(test_df, time_col='fecha', value_cols=names+cols)
@@ -87,8 +87,8 @@ test = transformer.transform(test)
 train = train.add_holidays(country_code='ES', prov='AN')
 
 # Definición de modelos
-epochs = 400
-batch = 256
+epochs = 200
+batch = 32
 
 mod_blockrnn = BlockRNNModel(
     model='RNN',
@@ -96,16 +96,15 @@ mod_blockrnn = BlockRNNModel(
     output_chunk_length=60,
     hidden_dim=25,
     n_rnn_layers=2,
-    dropout=0.1,
+    dropout=0.2,
     n_epochs=epochs,
     batch_size=batch,
     #optimizer_kwargs={"lr": 1e-3},
     show_warnings=True,
-    model_name='RNN',
+    model_name='rnn',
     #pl_trainer_kwargs={"precision": '64', "accelerator": "gpu", "devices": -1, "auto_select_gpus": True} 
     pl_trainer_kwargs={"precision": '64', "accelerator": "cpu"} 
 )
-
 mod_blockrnn_multi = cp(mod_blockrnn)
 
 mod_blocklstm = BlockRNNModel(
@@ -114,12 +113,12 @@ mod_blocklstm = BlockRNNModel(
     output_chunk_length=60,
     hidden_dim=25,
     n_rnn_layers=2,
-    dropout=0.1,
+    dropout=0.2,
     n_epochs=epochs,
     batch_size=batch,
     #optimizer_kwargs={"lr": 1e-3},
     show_warnings=True,
-    model_name='LSTM',
+    model_name='lstm',
     pl_trainer_kwargs={"precision": '64', "accelerator": "cpu"} 
 )
 mod_blocklstm_multi = cp(mod_blocklstm)
@@ -130,12 +129,12 @@ mod_blockgru = BlockRNNModel(
     output_chunk_length=60,
     hidden_dim=25,
     n_rnn_layers=2,
-    dropout=0.1,
+    dropout=0.2,
     n_epochs=epochs,
     batch_size=batch,
     #optimizer_kwargs={"lr": 1e-3},
     show_warnings=True,
-    model_name='GRU',
+    model_name='gru',
     pl_trainer_kwargs={"precision": '64', "accelerator": "cpu"} 
 )
 mod_blockgru_multi = cp(mod_blockgru)
@@ -149,68 +148,179 @@ mod_prophet = Prophet(
     floor=None
 )
 
-mod_neuralprophet = NeuralProphet()
-
 mod_nbeats = NBEATSModel(
     input_chunk_length=60,
     output_chunk_length=60,
-    dropout=0.1,
+    dropout=0.2,
     n_epochs=epochs,
     show_warnings=True,
     batch_size=batch,
-    model_name='LSTM',
+    model_name='nbeats',
     pl_trainer_kwargs={"precision": '64', "accelerator": "cpu"} 
 )
-
 mod_nbeats_multi = cp(mod_nbeats)
+
+mod_nhits = NHiTSModel(
+    input_chunk_length=60,
+    output_chunk_length=60,
+    dropout=0.2,
+    n_epochs=epochs,
+    show_warnings=True,
+    batch_size=batch,
+    model_name='nhits',
+    pl_trainer_kwargs={"precision": '64', "accelerator": "cpu"} 
+)
+mod_nhits_multi = cp(mod_nhits)
+
+mod_tcn = TCNModel(
+    input_chunk_length=120,
+    output_chunk_length=60,
+    dropout=0.2,
+    n_epochs=epochs,
+    show_warnings=True,
+    batch_size=batch,
+    model_name='tcn',
+    pl_trainer_kwargs={"precision": '64', "accelerator": "cpu"} 
+)
+mod_tcn_multi = cp(mod_tcn)
+
+mod_dlinear = DLinearModel(
+    input_chunk_length=60,
+    output_chunk_length=60,
+    n_epochs=epochs,
+    show_warnings=True,
+    batch_size=batch,
+    model_name='dlinear',
+    pl_trainer_kwargs={"precision": '64', "accelerator": "cpu"} 
+)
+mod_dlinear_multi = cp(mod_dlinear)
+
+mod_nlinear = NLinearModel(
+    input_chunk_length=60,
+    output_chunk_length=60,
+    n_epochs=epochs,
+    show_warnings=True,
+    batch_size=batch,
+    model_name='nlinear',
+    pl_trainer_kwargs={"precision": '64', "accelerator": "cpu"} 
+)
+mod_nlinear_multi = cp(mod_nlinear)
+
+mod_tide = TiDEModel(
+    input_chunk_length=60,
+    output_chunk_length=60,
+    dropout=0.2,
+    n_epochs=epochs,
+    show_warnings=True,
+    batch_size=batch,
+    model_name='tide',
+    pl_trainer_kwargs={"precision": '64', "accelerator": "cpu"} 
+)
+mod_tide_multi = cp(mod_tide)
+
+mod_tsmixer = TSMixerModel(
+    input_chunk_length=60,
+    output_chunk_length=60,
+    dropout=0.2,
+    n_epochs=epochs,
+    show_warnings=True,
+    batch_size=batch,
+    model_name='tide',
+    pl_trainer_kwargs={"precision": '64', "accelerator": "cpu"}     
+)
+mod_tsmixer_multi = cp(mod_tsmixer)
 
 def reset_models():
     mod_blockrnn.reset_model()
-    mod_blockrnn_multi.reset_model()
     mod_blocklstm.reset_model()
-    mod_blocklstm_multi.reset_model()
     mod_blockgru.reset_model()
-    mod_blockgru_multi.reset_model()
     mod_nbeats.reset_model()
-    mod_nbeats_multi.reset_model()
-    global mod_neuralprophet
-    mod_neuralprophet = NeuralProphet()
+    mod_nhits.reset_model()
+    mod_tcn.reset_model()
+    mod_dlinear.reset_model()
+    mod_nlinear.reset_model()
+    mod_tide.reset_model()
+    mod_tsmixer.reset_model()
 
+    mod_blockrnn_multi.reset_model()
+    mod_blocklstm_multi.reset_model()
+    mod_blockgru_multi.reset_model()
+    mod_nbeats_multi.reset_model()
+    mod_nhits_multi.reset_model()
+    mod_tcn_multi.reset_model()
+    mod_dlinear_multi.reset_model()
+    mod_nlinear_multi.reset_model()
+    mod_tide_multi.reset_model()
+    mod_tsmixer_multi.reset_model()
 
 # Entrenamiento
 for name in names:
     reset_models()
+    
+    print(name)    
+    mod_blockrnn.fit(train[f'{name}'])
+    mod_blockrnn.save(f'models/{name}/blockrnn')
 
-    # mod_blockrnn.fit(train[f'{name}'])
-    # mod_blockrnn.save(f'models/{name}/blockrnn')
+    mod_blocklstm.fit(train[f'{name}'])
+    mod_blocklstm.save(f'models/{name}/blocklstm')
 
-    # mod_blocklstm.fit(train[f'{name}'])
-    # mod_blocklstm.save(f'models/{name}/blocklstm')
+    mod_blockgru.fit(train[f'{name}'])
+    mod_blockgru.save(f'models/{name}/blockgru')
 
-    # mod_blockgru.fit(train[f'{name}'])
-    # mod_blockgru.save(f'models/{name}/blockgru')
+    mod_prophet.fit(train[f'{name}'])
+    mod_prophet.save(f'models/{name}/prophet')
 
-    # mod_prophet.fit(train[f'{name}'])
-    # mod_prophet.save(f'models/{name}/prophet')
+    mod_nbeats.fit(train[f'{name}'])
+    mod_nbeats.save(f'models/{name}/nbeats')
 
-    # mod_neuralprophet.fit(train_df[['fecha',f'{name}']].rename({'fecha':'ds', f'{name}':'y'}, axis=1), batch_size=8)
-    # utils.save(mod_neuralprophet, f'models/{name}/neuralprophet')
+    mod_nhits.fit(train[f'{name}'])
+    mod_nhits.save(f'models/{name}/nhits')
 
-    # mod_nbeats.fit(train[f'{name}'])
-    # mod_nbeats.save(f'models/{name}/nbeats')
+    mod_tcn.fit(train[f'{name}'])
+    mod_tcn.save(f'models/{name}/tcn')
 
-# mod_blockrnn_multi.fit(train)
-# mod_blockrnn_multi.save(f'models/multi/blockrnn_multi')
+    mod_dlinear.fit(train[f'{name}'])
+    mod_dlinear.save(f'models/{name}/dlinear')
 
-# mod_blocklstm_multi.fit(train)
-# mod_blocklstm_multi.save(f'models/multi/blocklstm_multi')
+    mod_nlinear.fit(train[f'{name}'])
+    mod_nlinear.save(f'models/{name}/nlinear')
 
-# mod_blockgru_multi.fit(train)
-# mod_blockgru_multi.save(f'models/multi/blockgru_multi')
+    mod_tide.fit(train[f'{name}'])
+    mod_tide.save(f'models/{name}/tide')
+    
+    mod_tsmixer.fit(train[f'{name}'])
+    mod_tsmixer.save(f'models/{name}/tsmixer')
 
-# mod_nbeats_multi.fit(train)
-# mod_nbeats_multi.save(f'models/multi/nbeats_multi')
 
+    mod_blockrnn_multi.fit(train[name, 'tmed', 'prec', 'hrMedia'])
+    mod_blockrnn_multi.save(f'models/{name}/blockrnn_multi')
 
+    mod_blocklstm_multi.fit(train[name, 'tmed', 'prec', 'hrMedia'])
+    mod_blocklstm_multi.save(f'models/{name}/blocklstm_multi')
 
-##### SCALER PARA NEURALPROPHET
+    mod_blockgru_multi.fit(train[name, 'tmed', 'prec', 'hrMedia'])
+    mod_blockgru_multi.save(f'models/{name}/blockgru_multi')
+
+    mod_prophet.fit(train[name, 'tmed', 'prec', 'hrMedia'])
+    mod_prophet.save(f'models/{name}/prophet')
+
+    mod_nbeats_multi.fit(train[name, 'tmed', 'prec', 'hrMedia'])
+    mod_nbeats_multi.save(f'models/{name}/nbeats_multi')
+
+    mod_nhits_multi.fit(train[name, 'tmed', 'prec', 'hrMedia'])
+    mod_nhits_multi.save(f'models/{name}/nhits_multi')
+
+    mod_tcn_multi.fit(train[name, 'tmed', 'prec', 'hrMedia'])
+    mod_tcn_multi.save(f'models/{name}/tcn_multi')
+
+    mod_dlinear_multi.fit(train[name, 'tmed', 'prec', 'hrMedia'])
+    mod_dlinear_multi.save(f'models/{name}/dlinear_multi')
+
+    mod_nlinear_multi.fit(train[name, 'tmed', 'prec', 'hrMedia'])
+    mod_nlinear_multi.save(f'models/{name}/nlinear_multi')
+
+    mod_tide_multi.fit(train[name, 'tmed', 'prec', 'hrMedia'])
+    mod_tide_multi.save(f'models/{name}/tide_multi')
+    
+    mod_tsmixer_multi.fit(train[name, 'tmed', 'prec', 'hrMedia'])
+    mod_tsmixer_multi.save(f'models/{name}/tsmixer_multi')
