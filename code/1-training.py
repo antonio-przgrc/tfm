60ed5a2af60a7804b43e57d3d621759634a061d5
Antonio Pérez García <antoniopgarcia95@gmail.com>
2024-08-29 19:52:43 +0200

    modificado entrenamiento

--- code/1-training.py
+++ code/1-training.py
@@ -1,11 +1,13 @@
-from copy import copy as cp
 import warnings
+import time
+import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 from darts import TimeSeries
 from darts.dataprocessing.transformers import Scaler
 from darts.models import Prophet, BlockRNNModel, NBEATSModel, NHiTSModel, TCNModel, DLinearModel, NLinearModel, TiDEModel, TSMixerModel
+from pytorch_lightning.callbacks import EarlyStopping
 
 warnings.filterwarnings('ignore')
 
@@ -75,12 +77,37 @@ series = series.add_holidays(country_code='ES', prov='AN')
 
 transformer = Scaler(scaler)
 series = transformer.fit_transform(series)
+series = series.astype(np.float32)
 
 train, test = series.split_after(pd.Timestamp(year=2023, month=12, day=31))
+_, val = train.split_after(pd.Timestamp(year=2022, month=12, day=31))
 
 # Definicion de modelos
 EPOCHS = 200
-BATCH = 256
+BATCH = 16
+INPUT = 260
+OUTPUT = 130
+DROPOUT = 0.2
+
+my_stopper = EarlyStopping(
+    monitor="train_loss",  
+    patience=10,
+    min_delta=0.0001,
+    mode='min',
+)
+
+pl_trainer_kwargs = {
+    "callbacks": [my_stopper],
+    "precision": '32',
+    "accelerator": "gpu",
+    "devices": -1,
+    "auto_select_gpus": True
+}
+
+encoders = {
+    'cyclic': {'past': ['quarter','dayofyear']},
+    'transformer': Scaler()
+}
 
 mod_blockrnn = BlockRNNModel(
     model='RNN',
@@ -92,11 +119,13 @@ mod_blockrnn = BlockRNNModel(
     n_epochs=EPOCHS,
     batch_size=BATCH,
     show_warnings=True,
-    model_name='rnn',
-    pl_trainer_kwargs={"precision": '64', "accelerator": "cpu"},
-    add_encoders={'cyclic': {'past': ['quarter','dayofyear']}}
+    model_name='RNN',
+    save_checkpoints=True,
+    log_tensorboard=True,
+    force_reset=True,
+    pl_trainer_kwargs=pl_trainer_kwargs,
+    add_encoders=encoders
 )
-mod_blockrnn_multi = cp(mod_blockrnn)
 
 mod_blocklstm = BlockRNNModel(
     model='LSTM',
@@ -108,11 +137,13 @@ mod_blocklstm = BlockRNNModel(
     n_epochs=EPOCHS,
     batch_size=BATCH,
     show_warnings=True,
-    model_name='lstm',
-    pl_trainer_kwargs={"precision": '64', "accelerator": "cpu"},
-    add_encoders={'cyclic': {'past': ['quarter','dayofyear']}}
+    model_name='LSTM',
+    save_checkpoints=True,
+    log_tensorboard=True,
+    force_reset=True,
+    pl_trainer_kwargs=pl_trainer_kwargs,
+    add_encoders=encoders
 )
-mod_blocklstm_multi = cp(mod_blocklstm)
 
 mod_blockgru = BlockRNNModel(
     model='GRU',
@@ -124,19 +155,21 @@ mod_blockgru = BlockRNNModel(
     n_epochs=EPOCHS,
     batch_size=BATCH,
     show_warnings=True,
-    model_name='gru',
-    pl_trainer_kwargs={"precision": '64', "accelerator": "cpu"},
-    add_encoders={'cyclic': {'past': ['quarter','dayofyear']}}
+    model_name='GRU',
+    save_checkpoints=True,
+    log_tensorboard=True,
+    force_reset=True,
+    pl_trainer_kwargs=pl_trainer_kwargs,
+    add_encoders=encoders
 )
-mod_blockgru_multi = cp(mod_blockgru)
 
 mod_prophet = Prophet(
     add_seasonalities=None, 
     country_holidays='ES', 
     suppress_stdout_stderror=True, 
-    add_encoders=None, 
-    cap=None, 
-    floor=None
+    force_reset=True,
+    pl_trainer_kwargs=pl_trainer_kwargs,
+    add_encoders=encoders
 )
 
 mod_nbeats = NBEATSModel(
@@ -146,11 +179,13 @@ mod_nbeats = NBEATSModel(
     n_epochs=EPOCHS,
     show_warnings=True,
     batch_size=BATCH,
-    model_name='nbeats',
-    pl_trainer_kwargs={"precision": '64', "accelerator": "gpu", "devices": -1, "auto_select_gpus": True},
-    add_encoders={'cyclic': {'past': ['quarter','dayofyear']}}
+    model_name='NBEATS',
+    save_checkpoints=True,
+    log_tensorboard=True,
+    force_reset=True,
+    pl_trainer_kwargs=pl_trainer_kwargs,
+    add_encoders=encoders
 )
-mod_nbeats_multi = cp(mod_nbeats)
 
 mod_nhits = NHiTSModel(
     input_chunk_length=260,
@@ -159,11 +194,13 @@ mod_nhits = NHiTSModel(
     n_epochs=EPOCHS,
     show_warnings=True,
     batch_size=BATCH,
-    model_name='nhits',
-    pl_trainer_kwargs={"precision": '64', "accelerator": "gpu", "devices": -1, "auto_select_gpus": True},
-    add_encoders={'cyclic': {'past': ['quarter','dayofyear']}}
+    model_name='NHiTS',
+    save_checkpoints=True,
+    log_tensorboard=True,
+    force_reset=True,
+    pl_trainer_kwargs=pl_trainer_kwargs,
+    add_encoders=encoders
 )
-mod_nhits_multi = cp(mod_nhits)
 
 mod_tcn = TCNModel(
     input_chunk_length=260,
@@ -172,11 +209,13 @@ mod_tcn = TCNModel(
     n_epochs=EPOCHS,
     show_warnings=True,
     batch_size=BATCH,
-    model_name='tcn',
-    pl_trainer_kwargs={"precision": '64', "accelerator": "gpu", "devices": -1, "auto_select_gpus": True},
-    add_encoders={'cyclic': {'past': ['quarter','dayofyear']}}
+    model_name='TCN',
+    save_checkpoints=True,
+    log_tensorboard=True,
+    force_reset=True,
+    pl_trainer_kwargs=pl_trainer_kwargs,
+    add_encoders=encoders
 )
-mod_tcn_multi = cp(mod_tcn)
 
 mod_dlinear = DLinearModel(
     input_chunk_length=260,
@@ -184,11 +223,13 @@ mod_dlinear = DLinearModel(
     n_epochs=EPOCHS,
     show_warnings=True,
     batch_size=BATCH,
-    model_name='dlinear',
-    pl_trainer_kwargs={"precision": '64', "accelerator": "gpu", "devices": -1, "auto_select_gpus": True},
-    add_encoders={'cyclic': {'future': ['quarter','dayofyear']}}
+    model_name='DLinear',
+    save_checkpoints=True,
+    log_tensorboard=True,
+    force_reset=True,
+    pl_trainer_kwargs=pl_trainer_kwargs,
+    add_encoders=encoders
 )
-mod_dlinear_multi = cp(mod_dlinear)
 
 mod_nlinear = NLinearModel(
     input_chunk_length=260,
@@ -196,11 +237,13 @@ mod_nlinear = NLinearModel(
     n_epochs=EPOCHS,
     show_warnings=True,
     batch_size=BATCH,
-    model_name='nlinear',
-    pl_trainer_kwargs={"precision": '64', "accelerator": "gpu", "devices": -1, "auto_select_gpus": True},
-    add_encoders={'cyclic': {'future': ['quarter','dayofyear']}}
+    model_name='NLinear',
+    save_checkpoints=True,
+    log_tensorboard=True,
+    force_reset=True,
+    pl_trainer_kwargs=pl_trainer_kwargs,
+    add_encoders=encoders
 )
-mod_nlinear_multi = cp(mod_nlinear)
 
 mod_tide = TiDEModel(
     input_chunk_length=260,
@@ -209,11 +252,13 @@ mod_tide = TiDEModel(
     n_epochs=EPOCHS,
     show_warnings=True,
     batch_size=BATCH,
-    model_name='tide',
-    pl_trainer_kwargs={"precision": '64', "accelerator": "gpu", "devices": -1, "auto_select_gpus": True},
-    add_encoders={'cyclic': {'future': ['quarter','dayofyear']}}
+    model_name='TiDE',
+    save_checkpoints=True,
+    log_tensorboard=True,
+    force_reset=True,
+    pl_trainer_kwargs=pl_trainer_kwargs,
+    add_encoders=encoders
 )
-mod_tide_multi = cp(mod_tide)
 
 mod_tsmixer = TSMixerModel(
     input_chunk_length=260,
@@ -222,101 +267,50 @@ mod_tsmixer = TSMixerModel(
     n_epochs=EPOCHS,
     show_warnings=True,
     batch_size=BATCH,
-    model_name='tsmixer',
-    pl_trainer_kwargs={"precision": '64', "accelerator": "gpu", "devices": -1, "auto_select_gpus": True},
-    add_encoders={'cyclic': {'future': ['quarter','dayofyear']}}
+    model_name='TSMixer',
+    save_checkpoints=True,
+    log_tensorboard=True,
+    force_reset=True,
+    pl_trainer_kwargs=pl_trainer_kwargs,
+    add_encoders=encoders
 )
-mod_tsmixer_multi = cp(mod_tsmixer)
+
+models = [
+    mod_blockrnn,
+    mod_blocklstm,
+    mod_blockgru,
+    mod_nbeats,
+    mod_nhits,
+    mod_tcn,
+    mod_dlinear,
+    mod_nlinear,
+    mod_tide,
+    mod_tsmixer,
+]
+
 
 def reset_models():
-    mod_blockrnn.reset_model()
-    mod_blocklstm.reset_model()
-    mod_blockgru.reset_model()
-    mod_nbeats.reset_model()
-    mod_nhits.reset_model()
-    mod_tcn.reset_model()
-    mod_dlinear.reset_model()
-    mod_nlinear.reset_model()
-    mod_tide.reset_model()
-    mod_tsmixer.reset_model()
+    for model in models:
+        model.reset_model()
 
-    mod_blockrnn_multi.reset_model()
-    mod_blocklstm_multi.reset_model()
-    mod_blockgru_multi.reset_model()
-    mod_nbeats_multi.reset_model()
-    mod_nhits_multi.reset_model()
-    mod_tcn_multi.reset_model()
-    mod_dlinear_multi.reset_model()
-    mod_nlinear_multi.reset_model()
-    mod_tide_multi.reset_model()
-    mod_tsmixer_multi.reset_model()
+tiempo_ejecucion = pd.DataFrame()
 
 # Entrenamiento
-for name in names:
-    reset_models()
-    print(name)
+# for name in names:
+#     reset_models()
+#     print(name)
 
-    # Modelos sin covariates
-    mod_blockrnn.fit(train[f'{name}'])
-    mod_blockrnn.save(f'models/{name}/blockrnn')
+for model in models:
+    print(model.model_name)
+    my_stopper.wait_count = 0 # Reinicio de patience
 
-    mod_blocklstm.fit(train[f'{name}'])
-    mod_blocklstm.save(f'models/{name}/blocklstm')
+    tiempo1 = time.time()
+    if model.supports_future_covariates:
+        model.fit(series=train[names], past_covariates=train[['tmed', 'prec', 'hrMedia', 'gasolina', 'diesel']], future_covariates=series['holidays'], dataloader_kwargs={"num_workers": 12}, val_past_covariates=val)
+    else:
+        model.fit(series=train[names], past_covariates=train.drop_columns(names), dataloader_kwargs={"num_workers": 12}, val_past_covariates=val)
+    tiempo_ejecucion["tiempo"] = {model.model_name: (time.time() - tiempo1)}
 
-    mod_blockgru.fit(train[f'{name}'])
-    mod_blockgru.save(f'models/{name}/blockgru')
+    # model.save(f'models/{name}/{model.model_name}')
 
-    mod_prophet.fit(train[f'{name}'])
-    mod_prophet.save(f'models/{name}/prophet')
-
-    mod_nbeats.fit(train[f'{name}'])
-    mod_nbeats.save(f'models/{name}/nbeats')
-
-    mod_nhits.fit(train[f'{name}'])
-    mod_nhits.save(f'models/{name}/nhits')
-
-    mod_tcn.fit(train[f'{name}'])
-    mod_tcn.save(f'models/{name}/tcn')
-
-    mod_dlinear.fit(train[f'{name}'])
-    mod_dlinear.save(f'models/{name}/dlinear')
-
-    mod_nlinear.fit(train[f'{name}'])
-    mod_nlinear.save(f'models/{name}/nlinear')
-
-    mod_tide.fit(train[f'{name}'])
-    mod_tide.save(f'models/{name}/tide')
-
-    mod_tsmixer.fit(train[f'{name}'])
-    mod_tsmixer.save(f'models/{name}/tsmixer')
-
-    # Modelos con covariates
-    mod_blockrnn_multi.fit(series=train[name], past_covariates=train.drop_columns(names))
-    mod_blockrnn_multi.save(f'models/{name}/blockrnn_multi')
-
-    mod_blocklstm_multi.fit(series=train[name], past_covariates=train.drop_columns(names))
-    mod_blocklstm_multi.save(f'models/{name}/blocklstm_multi')
-
-    mod_blockgru_multi.fit(series=train[name], past_covariates=train.drop_columns(names))
-    mod_blockgru_multi.save(f'models/{name}/blockgru_multi')
-
-    mod_nbeats_multi.fit(series=train[name], past_covariates=train.drop_columns(names))
-    mod_nbeats_multi.save(f'models/{name}/nbeats_multi')
-
-    mod_nhits_multi.fit(series=train[name], past_covariates=train.drop_columns(names))
-    mod_nhits_multi.save(f'models/{name}/nhits_multi')
-
-    mod_tcn_multi.fit(series=train[name], past_covariates=train.drop_columns(names))
-    mod_tcn_multi.save(f'models/{name}/tcn_multi')
-
-    mod_dlinear_multi.fit(series=train[name], past_covariates=train[['tmed', 'prec', 'hrMedia', 'gasolina', 'diesel']], future_covariates=series['holidays'])
-    mod_dlinear_multi.save(f'models/{name}/dlinear_multi')
-
-    mod_nlinear_multi.fit(series=train[name], past_covariates=train[['tmed', 'prec', 'hrMedia', 'gasolina', 'diesel']], future_covariates=series['holidays'])
-    mod_nlinear_multi.save(f'models/{name}/nlinear_multi')
-
-    mod_tide_multi.fit(series=train[name], past_covariates=train[['tmed', 'prec', 'hrMedia', 'gasolina', 'diesel']], future_covariates=series['holidays'])
-    mod_tide_multi.save(f'models/{name}/tide_multi')
-    
-    mod_tsmixer_multi.fit(series=train[name], past_covariates=train[['tmed', 'prec', 'hrMedia', 'gasolina', 'diesel']], future_covariates=series['holidays'])
-    mod_tsmixer_multi.save(f'models/{name}/tsmixer_multi')
+tiempo_ejecucion.to_csv('results/tiempos.csv')
